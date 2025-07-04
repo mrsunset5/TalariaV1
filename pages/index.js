20d41153faf25d0a456cb336f7028aa9d28d3f61
mrsunset5 <matthewsunset@talaria.info>
2025-07-03 23:23:18 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-3xl font-bold">ClareVOne is live</h1>
+    </div>
+  );
+}
 
